81b9f0c4761004ab9a09a93c75e4aa3923df6869
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-02-20 20:18:21 +0530

    Compute Employee Wage for multiple Companies

---
+++ EmpWage.java
@@ -0,0 +1,41 @@
+package com.bl.employeeWage;
+
+import java.util.Random;
+
+public class EmpWage {
+    public static final int partTime = 1;
+    public static final int fullTime = 2;
+    public void method(String Company , int RatePerHr, int WorkingDays, int workingHrs){
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0; int totalEmpHrs = 0;
+        int day =0;
+        while(totalEmpHrs <= workingHrs &&  day < WorkingDays){
+            Random rd = new Random();
+            int a = rd.nextInt(3);
+            switch (a) {
+                case partTime:
+                    empHrs = 4;
+                    break;
+                case fullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * RatePerHr;
+            totalEmpWage = totalEmpWage + empWage;
+            totalEmpHrs = totalEmpHrs + empHrs;
+            day++;
+        }
+        System.out.println("Total Number Of Hours: "+totalEmpHrs);
+        System.out.println("Total Employee Wage: "+totalEmpWage);
+    }
+    public static void main(String[] args) {
+        EmpWage obj = new EmpWage();
+        System.out.println("Employee Wage of 1st Company");
+        obj.method("Airtel", 20, 2, 10);
+        System.out.println("Employee Wage of 2nd Company");
+        obj.method("Jio", 10, 3,20);
+    }
+}
