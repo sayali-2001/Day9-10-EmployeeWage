f90657b9dfa90d18a94c48d70c62414edf02fb01
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-02-20 20:02:41 +0530

    Add Part time Employee & Wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,18 +3,28 @@ package com.bl.employeeWage;
 import java.util.Random;
 
 public class EmpWage {
-    int fullTime = 1;
 
+    int partTime = 1;
+    int fullTime = 2;
+    int emp_rate_per_hr = 20;
+    int empHrs = 0;
+    int empWage = 0;
     public void empcheck(){
         Random rd = new Random();
-        int present = rd.nextInt(2);
-        if(present == fullTime){
-            System.out.println("Employee is Present");
+        int present = rd.nextInt(3);
+        if(present == fullTime)
+        {
+            empHrs = 16;
+        } else if(present == partTime)
+        {
+            empHrs = 8;
+        } else{
+            empHrs = 0;
         }
-        else{
-            System.out.println("Employee is Absent");
-        }
+        empWage = empHrs * emp_rate_per_hr;
+        System.out.println("Daily Employee Wage : "+empWage);
     }
+
     public static void main(String[] args) {
         EmpWage obj = new EmpWage();
         obj.empcheck();
